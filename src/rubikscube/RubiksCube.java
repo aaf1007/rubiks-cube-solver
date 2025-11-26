0da07eb7e5044fca3bf2aaa6e5fc22555500683f
anton <antonflorendo7@gmail.com>
2025-11-26 14:47:59 -0800

    optimized rubikscube class

--- src/rubikscube/RubiksCube.java
+++ src/rubikscube/RubiksCube.java
@@ -20,7 +20,7 @@ public class RubiksCube {
      *    RRR
      */
 
-    private char[][] cube; // 2D array of the cube
+    private byte[][] cube; // 2D array of the cube
     private static final String SOLVED_STATE =
             "   OOO\n" +
             "   OOO\n" +
@@ -72,7 +72,7 @@ public class RubiksCube {
      * @param other
      */
     public RubiksCube(RubiksCube other) {
-        this.cube = new char[9][12];
+        this.cube = new byte[9][12];
         for (int i = 0; i < 9; i++) {
             System.arraycopy(other.cube[i], 0, this.cube[i], 0, 12);
         }
@@ -118,7 +118,7 @@ public class RubiksCube {
         // Check valid colors
         String validColors = "WYRGBO";
         for (String line : lines) {
-            for (char c : line.toCharArray()) {
+            for (byte c : line.getBytes()) {
                 if (c != ' ' && validColors.indexOf(c) == -1) {
                     throw new IncorrectFormatException("Invalid color: " + c);
                 }
@@ -130,12 +130,12 @@ public class RubiksCube {
      *  Initializes cube from string
      */
     private void initializeFromString(String str) {
-        cube = new char[9][12];
+        cube = new byte[9][12];
         String[] lines = str.split("\n");
         for (int i = 0; i < 9; i++) {
             String line = lines[i];
             for (int j = 0; j < line.length(); j++) {
-                cube[i][j] = line.charAt(j);
+                cube[i][j] = (byte) line.charAt(j);
             }
             // Fill remaining with spaces if line is shorter
             for (int j = line.length(); j < 12; j++) {
@@ -147,66 +147,86 @@ public class RubiksCube {
     /**
      * Rotates a 3x3 face clockwise
      */
-    private void rotateFace(int startRow, int startCol) {
-        char[][] temp = new char[3][3];
+    private void rotateFace(int row, int col) {
+        // byte[][] temp = new byte[3][3];
         
-        // Copy face to temp
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                temp[i][j] = cube[startRow + i][startCol + j];
-            }
-        }
+        // // Copy face to temp
+        // for (int i = 0; i < 3; i++) {
+        //     for (int j = 0; j < 3; j++) {
+        //         temp[i][j] = cube[startRow + i][startCol + j];
+        //     }
+        // }
         
-        // Rotate clockwise: [i][j] -> [j][2-i]
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                cube[startRow + j][startCol + 2 - i] = temp[i][j];
-            }
-        }
-    }
+        // // Rotate clockwise: [i][j] -> [j][2-i]
+        // for (int i = 0; i < 3; i++) {
+        //     for (int j = 0; j < 3; j++) {
+        //         cube[startRow + j][startCol + 2 - i] = temp[i][j];
+        //     }
+        // }
 
+        // Updated Implementaion with optimized memory allocations
+
+        // 1. Rotate Corners
+        byte temp = cube[row][col];
+
+        // TopLeft - BottomLeft
+        cube[row][col] = cube[row+2][col];
+        // BottomLeft - BottomRight 
+        cube[row + 2][col] = cube[row + 2][col + 2];
+        // BottomRight - TopRight
+        cube[row + 2][col + 2] = cube[row][col + 2]; 
+        // TopRight - TopLeft (temp)
+        cube[row][col + 2] = temp; 
+
+        // 2. Rotate Edges
+        temp = cube [row][col+1];
+
+        // TopMid - LeftMid
+        cube[row][col+1] = cube[row+1][col];
+        // LeftMid - BottomMid
+        cube[row + 1][col] = cube[row + 2][col + 1]; 
+        // BottomMid - RightMid
+        cube[row + 2][col + 1] = cube[row + 1][col + 2]; 
+        // RightMid - TopMid (temp)   
+        cube[row + 1][col + 2] = temp;         
+    }
     /**
      * @param moves - String of moves
      * Applies the sequence of moves on the Rubik's Cube
      */
-    // public void applyMoves(String moves) {
-    //     // TODO implement me
-    //     // For all loop to call applyMove(char)
+    public void applyMoves(String moves) {
+        // TODO implement me
+        // For all loop to call applyMove(byte)
 
-    //     String validMoves = "FBRLUD";
-    //     char[] move = moves.toCharArray();
+        char[] move = moves.toCharArray();
 
-    //     for (int i = 0; i < move.length; i++) {
-    //         if (i+1 < move.length && move[i+1] == '\'') {
-    //             // Counter-clockwise rotation so call it 3 times
-    //             for (int j = 0; j < 3; j++)
-    //                 applyMove(move[i]);
-
-    //             i++; // Skip \'
-    //             continue;
-    //         }
-
-    //         if (move[i] == '\'')
-    //             continue;
+        for (int i = 0; i < move.length; i++) {
+            if (Character.isLowerCase(move[i])) {
+                // Counter-clockwise rotation so call it 3 times
+                for (int j = 0; j < 3; j++)
+                    applyMove(Character.toUpperCase(move[i]));
+                
+                continue;
+            }
             
-    //         applyMove(move[i]);
-    //     }
-    // }
+            applyMove(move[i]);
+        }
+    }
 
     /**
      * For applying one moves
-     * @param char of single move
+     * @param byte of single move
      */
-     private void applyMove(char move) {
+     public void applyMove(char move) {
         switch (move) {
-            case 'F':
+            case 'F': {
                 rotateFace(3, 3); // White face
                 // Save edge pieces that will be moved
-                char[] tempF = new char[3];
+
                 // Save bottom row of Up face
-                tempF[0] = cube[2][3];
-                tempF[1] = cube[2][4];
-                tempF[2] = cube[2][5];
+                byte t1 = cube[2][3];
+                byte t2 = cube[2][4];
+                byte t3 = cube[2][5];
                 
                 // Up bottom row <- Left right column
                 cube[2][3] = cube[5][2];
@@ -224,18 +244,19 @@ public class RubiksCube {
                 cube[6][5] = cube[3][6];
                 
                 // Right left column <- saved Up bottom row
-                cube[3][6] = tempF[0];
-                cube[4][6] = tempF[1];
-                cube[5][6] = tempF[2];
+                cube[3][6] = t1;
+                cube[4][6] = t2;
+                cube[5][6] = t3;
                 break;
-                
-            case 'B':
+            }
+
+            case 'B': {
                 rotateFace(3, 9); // Yellow face
-                char[] tempB = new char[3];
+
                 // Save top row of Up face
-                tempB[0] = cube[0][3];
-                tempB[1] = cube[0][4];
-                tempB[2] = cube[0][5];
+                byte t1 = cube[0][3];
+                byte t2 = cube[0][4];
+                byte t3 = cube[0][5];
                 
                 // Up top row <- Right right column
                 cube[0][3] = cube[3][8];
@@ -253,19 +274,19 @@ public class RubiksCube {
                 cube[8][5] = cube[3][0];
                 
                 // Left left column <- saved Up top row
-                cube[3][0] = tempB[2];
-                cube[4][0] = tempB[1];
-                cube[5][0] = tempB[0];
+                cube[3][0] = t3;
+                cube[4][0] = t2;
+                cube[5][0] = t1;
                 break;
-                
-            case 'R': 
+            }
+            
+            case 'R': {
                 rotateFace(3, 6); // Blue face
-                char[] tempR = new char[3];
 
                 // Save Back left column <- temp
-                tempR[0] = cube[3][9];
-                tempR[1] = cube[4][9];
-                tempR[2] = cube[5][9];
+                byte t1 = cube[3][9];
+                byte t2 = cube[4][9];
+                byte t3 = cube[5][9];
                 // Back left column <- Top right column
                 cube[5][9] = cube [0][5];
                 cube[4][9] = cube [1][5];
@@ -279,18 +300,19 @@ public class RubiksCube {
                 cube[4][5] = cube[7][5];
                 cube[5][5] = cube[8][5];
                 // Bottom right col <- saved temp
-                cube[6][5] = tempR[2];
-                cube[7][5] = tempR[1];
-                cube[8][5] = tempR[0];
+                cube[6][5] = t3;
+                cube[7][5] = t2;
+                cube[8][5] = t1;
                 break;
-                
-            case 'L':
+            }
+
+            case 'L': {
                 rotateFace(3, 0); // Green face
-                char[] tempL = new char[3];
+
                 // Save front left column
-                tempL[0] = cube[3][3];
-                tempL[1] = cube[4][3];
-                tempL[2] = cube[5][3];
+                byte t1 = cube[3][3];
+                byte t2 = cube[4][3];
+                byte t3 = cube[5][3];
                 
                 // Front left column <- Up left column
                 cube[3][3] = cube[0][3];
@@ -308,18 +330,19 @@ public class RubiksCube {
                 cube[5][11] = cube[6][3];
                 
                 // Down left column <- saved Front left column
-                cube[6][3] = tempL[0];
-                cube[7][3] = tempL[1];
-                cube[8][3] = tempL[2];
+                cube[6][3] = t1;
+                cube[7][3] = t2;
+                cube[8][3] = t3;
                 break;
-                
-            case 'U': 
+            }
+
+            case 'U': {
                 rotateFace(0, 3); // Orange face
-                char[] tempU = new char[3];
+
                 // Save Front top row
-                tempU[0] = cube[3][3];
-                tempU[1] = cube[3][4];
-                tempU[2] = cube[3][5];
+                byte t1 = cube[3][3];
+                byte t2 = cube[3][4];
+                byte t3 = cube[3][5];
                 // Front top row <- Right top row
                 cube[3][3] = cube[3][6];
                 cube[3][4] = cube[3][7];
@@ -333,18 +356,19 @@ public class RubiksCube {
                 cube[3][10] = cube[3][1];
                 cube[3][11] = cube[3][2];
                 // Left top row <- saved temp
-                cube[3][0] = tempU[0];
-                cube[3][1] = tempU[1];
-                cube[3][2] = tempU[2];
+                cube[3][0] = t1;
+                cube[3][1] = t2;
+                cube[3][2] = t3;
                 break;
-                  
-            case 'D':
+            }
+
+            case 'D': {
                 rotateFace(6, 3); // Red face
-                char[] tempD = new char[3];
+
                 // Save front bottom row
-                tempD[0] = cube[5][3];
-                tempD[1] = cube[5][4];
-                tempD[2] = cube[5][5];
+                byte t1 = cube[5][3];
+                byte t2 = cube[5][4];
+                byte t3 = cube[5][5];
                 
                 // Front bottom row <- Left bottom row
                 cube[5][3] = cube[5][0];
@@ -362,11 +386,22 @@ public class RubiksCube {
                 cube[5][11] = cube[5][8];
                 
                 // Right bottom row <- saved Front bottom row
-                cube[5][6] = tempD[0];
-                cube[5][7] = tempD[1];
-                cube[5][8] = tempD[2];
+                cube[5][6] = t1;
+                cube[5][7] = t2;
+                cube[5][8] = t3;
                 break;
+            }
         }
+    }
+
+    /**
+     * 
+     * @param row
+     * @param col
+     * @return byte color
+     */
+    public byte getColor(int row, int col) {
+        return cube[row][col];
     }
 
     /**
@@ -390,12 +425,12 @@ public class RubiksCube {
     @Override
     public String toString() {
         // TODO implement me
-        // Make 2D char array a string
+        // Make 2D byte array a string
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 12; j++) {
                 if (cube[i][j] != '\0') {
-                    result.append(cube[i][j]);
+                    result.append((char) cube[i][j]);
                 }
             }
             // Trim trailing spaces for rows 0-2 and 6-8
@@ -439,7 +474,7 @@ public class RubiksCube {
     /**
      * Get internal cube array (for heuristic calculations)
      */
-    public char[][] getCube() {
+    public byte[][] getCube() {
         return cube;
     }
 
